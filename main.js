1a8644e1590be57408c4a3f8e3f54f43947302df
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-06-05 00:39:30 -0400

    update 'testimonials' section => add swiper js pagination and responsive design

--- main.js
+++ main.js
@@ -18,10 +18,23 @@ closeNavBtn.addEventListener('click', closeNav);
 
 // Testimonials Section (swiper js)
 var swiper = new Swiper('.mySwiper', {
-	slidesPerView: 3,
+	slidesPerView: 1,
 	spaceBetween: 30,
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
+	},
+
+	// Responsive Breakpoints
+	breakpoints: {
+		// When window width is >= 600px
+		600: {
+			slidesPerView: 2,
+		},
+
+		// When window width is >= 1024px
+		1024: {
+			slidesPerView: 3,
+		},
 	},
 });
